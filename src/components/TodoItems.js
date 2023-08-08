94249b662619fe50e5c9d49b924b2cacdf0e4903
prince475 <danielmwai475@gmail.com>
2023-08-08 22:23:14 +0300

    Creating TodoItems component.

---
+++ src/components/TodoItems.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function TodoItems({entries, deleteItem}) {
+
+
+  return (
+    <ul className='mt-3'>
+        <b className='ms-3'>Items:</b>
+        {entries.map(({ id, text }) => {
+            return(
+                <li
+                className='px-2 mt-1 ms-2'
+                key={i}
+                onClick={() =>{
+                    deleteItem(id)
+                }}
+                >{text}</li>
+            )
+        })}
+    </ul>
+  )
+}
+
